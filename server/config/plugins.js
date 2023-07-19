ae42c21f6428374b560b7540e3d0c1b93ca8e87f
Akam <mr.akam.320@gmail.com>
2023-07-20 00:52:09 +0330

    add strapi to server directory

---
+++ server/config/plugins.js
@@ -0,0 +1,30 @@
+module.exports = ({ env }) => ({
+    tinymce: {
+        config: {
+            editor: {
+                outputFormat: "html",
+                editorConfig: {
+                    language: 'fa_IR',
+                    language_url: 'https://cdn.tiny.cloud/1/langs/fa_IR.js',
+                    height: 500,
+                    extended_valid_elements: "span, img, small",
+                    plugins: 'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
+                    toolbar: 'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+                    codesample_languages: [
+                        { text: 'HTML/XML', value: 'markup' },
+                        { text: 'JavaScript', value: 'javascript' },
+                        { text: 'TypeScrpit', value: 'typescript' },
+                        { text: 'CSS', value: 'css' },
+                        { text: 'PHP', value: 'php' },
+                        { text: 'Ruby', value: 'ruby' },
+                        { text: 'Python', value: 'python' },
+                        { text: 'Java', value: 'java' },
+                        { text: 'C', value: 'c' },
+                        { text: 'C#', value: 'csharp' },
+                        { text: 'C++', value: 'cpp' }
+                    ],
+                },
+            },
+        },
+    },
+});
